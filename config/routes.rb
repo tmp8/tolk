57cdd49b86ae441bbc30def0a8aed4582f74cd81
unnu <norman.timmler@gmail.com>
2011-05-17 15:55:35 +0200

    * fixed missing paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace('tolk') do
     root :to => 'locales#index'
-    resources :locales, :member => {:all => :get, :updated => :get}
+    resources :locales do
+      member do
+        get :all
+        get :updated
+      end
+    end
     resource :search
   end
 end
